0bd74f6a505677f4d66aa9610c8c7bdc061ff70b
miamiww <rivendalejones@gmail.com>
2018-10-09 11:36:55 -0400

    adding adventure start

--- talk_adventure/functions/index.js
+++ talk_adventure/functions/index.js
@@ -1,4 +1,5 @@
 const functions = require('firebase-functions');
+const random = require("random-js")();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,29 +8,87 @@ const functions = require('firebase-functions');
 //  response.send("Hello from Firebase!");
 // });
 
-app.intent('Default Welcome Intent', conv => {
-    conv.ask('You are standing at the end of a road before a small brick building. Around you is a forest. A small stream flows out of the building and down a gully.');
+// set defaults
+// note to self: this could be better handled with an object
+var room = "End of Road";
+var value = random.integer(1,20);
+var items = [];
+var roomItems = [];
+var description = "You are standing at the end of a road before a small brick building. Around you is a forest. A small stream flows out of the building and down a gully.";
 
-});
-
-app.intent('get fortune', (conv, params) => {
-    // convs.contexts.set('dreams question', 5);
-    conv.ask(`Hello, ${params.name}, what did you dream about last night?`);
-
-});
-
-app.intent('dreams', (conv) => {
-    const dreamSentiment = sentiment.analyze(conv.query)
-    console.log(dreamSentiment);
-    let day;
-    if(dreamSentiment.score < -2){
-        day = ' you are going to have a terrible day'
-    } else if(dreamSentiment.score >= -2 && dreamSentiment < 2){
-        day = 'your day will be fine'
-    } else {
-        day = 'you are going to have the best day ever'
+const moveR = (room, direction) => {
+    if (room = "End of Road"){
+        if(direction = "north"){
+            return('Building');
+        } else {
+            return('Forest');
+        }
     }
-    conv.close(`${day}`);
+
+    if (room = "Forest"){
+        return('Forest');
+    }
+    if (room = "Building"){
+        if(direction = "south"){
+            return('End of Road');
+        } else{
+            return('Building');
+        }
+    }
+}
+
+const checkRoom = room => {
+    if(room = "End of Road"){
+        roomItems = [];
+        description = "You are standing at the end of a road before a small brick building. Around you is a forest. A small stream flows out of the building and down a gully.";
+    }
+    if(room = "Forest"){
+        roomItems = [];
+        description = "You are lost in the forest."
+    }
+    if(room = "Building"){
+        roomItems = ["lamp", "food","bottle"];
+        description = ["You are standing in the small building. There is a clear stream of water running through it."]
+    }
+}
+
+
+app.intent('Default Welcome Intent', conv => {
+    conv.ask(`${description}`);
+
+});
+
+app.intent('look', conv => {
+    if(roomItems = []){
+        conv.ask(`${description}`);
+    } else {
+        conv.ask(`${description} There is a ${roomItems[0]}, ${roomItems[1]}, and ${roomItems[2]} there.`)
+    }
+
+});
+
+app.intent('take', (conv, params) => {
+    if(roomItems = []){
+        conv.ask("There is nothing to take.");
+    } else {
+        items = roomItems;
+        roomItems = [];
+        conv.ask("You have taken the items.")
+    }
+});
+
+app.intent('move', (conv, params) => {
+
+    room = moveR(room,params.direction);
+    roomItems = [];
+});
+
+
+app.intent('quit', (conv) => {
+
+    conv.close("Goodbye");
 })
 
+
+//fix this
 exports.Experiment = functions.https.onRequest(app);

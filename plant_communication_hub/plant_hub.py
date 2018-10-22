2ba40c5f23aadd1437ecb10ac277a4e669bbf52f
miamiww <rivendalejones@gmail.com>
2018-10-22 17:53:47 -0400

    adding some better scripting

--- plant_communication_hub/plant_hub.py
+++ plant_communication_hub/plant_hub.py
@@ -8,9 +8,9 @@ import serial
 import os
 import threading
 
-plant_chat = ['gregorios are alll here']
+plant_chat = ['gregorios is alll here']
 plant_response = ['hello human my good friend you are here and speaking to me again it is so good when I hear your voice I wish to cry out in joy here I am crying out in JOY I AM SO HAPPY YES IT IS good TO BE HERE A plant IN THIS HOME']
-
+water_status = ['water is good I love water and my friend Alden brings me water', 'water', 'water is good for my roots', 'this water is good and I like the humidity in the air']
 
 def main():
     recognizer = aiy.cloudspeech.get_recognizer()
@@ -20,7 +20,7 @@ def main():
     aiy.audio.get_recorder().start()
 
     while True:
-        aiy.audio.say('Hello')
+        aiy.audio.say('Hello Alden God of Water and my good friend I want to again wish YOU happy birthday and to thank you for taking me on this trip')
 #        while True:
 #            os.system('aplay soloKnocks.wav')
 #            reading = ser.read(1)
@@ -41,6 +41,8 @@ def main():
             #     ser.flush()
             # elif 'turn off the light' in text:
             #     led.set_state(aiy.voicehat.LED.OFF)
+            if 'water' in text:
+                aiy.audio.say(random.choice(water_status))
             if 'hello' in text:
                 aiy.audio.say(random.choice(plant_response))
             elif 'goodbye' in text:
